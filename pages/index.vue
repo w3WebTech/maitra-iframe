39a4266a7c1129e34c24f5212754b6c5dfded715
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2025-03-12 14:26:32 +0530

    fix

---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+    
+      <div ref="" class="iframe-container">
+        <iframe
+          ref="iframe"
+          :src="iframeUrl"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        iframeUrl: 'https://pf.maitracommodities.com/maitra/login', // Replace with your desired URL
+      };
+    },
+    methods: {
+      toggleFullScreen() {
+        const iframeContainer = this.$refs.iframeContainer;
+        if (!document.fullscreenElement) {
+          iframeContainer.requestFullscreen().catch(err => {
+            console.error(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+          });
+        } else {
+          document.exitFullscreen();
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+.iframe-container {
+  width: 100%;
+  height: 100vh; /* Set to full viewport height */
+  position: relative;
+}
+
+iframe {
+  width: 100%;
+  height: 100%; /* Take full height of the container */
+  border: none;
+}
+  </style>
