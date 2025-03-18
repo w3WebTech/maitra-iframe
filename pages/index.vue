6fb11fb335541804c3c6c5eaaf322e1b410c13f1
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2025-03-18 12:29:43 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,47 @@
 <template>
-    <div>
-    
-      <div ref="" class="iframe-container">
-        <iframe
-          ref="iframe"
-          :src="iframeUrl"
-          frameborder="0"
-          allowfullscreen
-        ></iframe>
-      </div>
+  <div>
+    <div ref="iframeContainer" class="iframe-container">
+      <iframe
+        ref="iframe"
+        :src="iframeUrl"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        iframeUrl: 'https://pf.maitracommodities.com/maitra/sso-login/VIN001', // Replace with your desired URL
-      };
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      iframeUrl: '', // Initialize as an empty string
+    };
+  },
+  created() {
+    // Check if clientCode is available in the route parameters
+    const clientCode = this.$route.params.clientCode;
+    // Set the iframeUrl based on the clientCode
+    this.iframeUrl = clientCode 
+      ? `https://pf.maitracommodities.com/maitra/sso-login/${clientCode}` 
+      : 'https://pf.maitracommodities.com/maitra/sso-login/VIN001'; 
+  },
+  methods: {
+    toggleFullScreen() {
+      const iframeContainer = this.$refs.iframeContainer;
+      if (!document.fullscreenElement) {
+        iframeContainer.requestFullscreen().catch(err => {
+          console.error(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+        });
+      } else {
+        document.exitFullscreen();
+      }
     },
-    methods: {
-      toggleFullScreen() {
-        const iframeContainer = this.$refs.iframeContainer;
-        if (!document.fullscreenElement) {
-          iframeContainer.requestFullscreen().catch(err => {
-            console.error(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-          });
-        } else {
-          document.exitFullscreen();
-        }
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
+  },
+};
+</script>
+
+<style scoped>
 .iframe-container {
   width: 100%;
   height: 100vh; /* Set to full viewport height */
@@ -46,4 +53,4 @@ iframe {
   height: 100%; /* Take full height of the container */
   border: none;
 }
-  </style>
+</style>
